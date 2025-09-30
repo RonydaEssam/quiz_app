739d41de284224eee16142b942c81dbe7c51a759
RonydaEssam <ronyda988@gmail.com>
2025-09-30 21:33:32 +0300

    update start screen

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
                   ),
                 ),
                 child: Text(
-                  'Start',
+                  'Start Quiz',
                   style: TextStyle(
                     fontSize: 22,
                     color: Colors.black,
