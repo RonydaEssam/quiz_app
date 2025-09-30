42c7c1689d7cac1bc575055d76c4f32fb0df806f
RonydaEssam <ronyda988@gmail.com>
2025-09-30 21:27:54 +0300

    start screen

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,44 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Center(
-        child: Column(
-          children: [
-            Image.asset(
-              'assets/images/question-mark-1',
-            ),
-          ],
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/question-mark.png',
+              ),
+              Text(
+                'Ready for a challenge?',
+                style: TextStyle(
+                  fontSize: 26,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll(
+                    const Color.fromARGB(255, 201, 201, 201),
+                  ),
+                ),
+                child: Text(
+                  'Start',
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
