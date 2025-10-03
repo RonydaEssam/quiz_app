41921c976ea59f6445665d16d24deecaf08e3b7e
RonydaEssam <ronyda988@gmail.com>
2025-10-03 14:21:37 +0300

    updating from stateless to stateful widgets

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,16 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 
-class StartScreen extends StatelessWidget {
+class StartScreen extends StatefulWidget {
   const StartScreen({super.key});
 
+  @override
+  State<StartScreen> createState() {
+    return _StartScreenState();
+  }
+}
+
+class _StartScreenState extends State<StartScreen> {
   @override
   Widget build(BuildContext context) {
     return Center(
