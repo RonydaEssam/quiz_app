2461007c78545749f96c5c6994a06a2c76915c0f
RonydaEssam <ronyda988@gmail.com>
2025-10-05 15:36:09 +0300

    add buttons

--- lib/questions.dart
+++ lib/questions.dart
@@ -12,6 +12,27 @@ class Questions extends StatefulWidget {
 class _QuestionsState extends State<Questions> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Questions Screen');
+    return Column(
+      children: const [
+        Text('Questions Screen'),
+        SizedBox(height: 30),
+        ElevatedButton(
+          onPressed: () {},
+          child: Text('Answer 1'),
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          child: Text('Answer 2'),
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          child: Text('Answer 3'),
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          child: Text('Answer 4'),
+        ),
+      ],
+    );
   }
 }
